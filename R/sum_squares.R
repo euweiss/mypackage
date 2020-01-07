d51331b9033accd5200aae8939f35ab23de515a7
Eva Weiss <evaweiss88@gmail.com>
2020-01-07 12:22:05 +0000

    added documentation

--- R/sum_squares.R
+++ R/sum_squares.R
@@ -1,10 +1,15 @@
-############################################
-#
-# Calculates the sum of squares of vector
-# value.
-#
-############################################
-
+#' Calculate the sums if squares of a sample
+#'
+#' Returns the sum of the squared deviations
+#' from mean, SS(x) of a numeric vector
+#'
+#' @param value numeric vector
+#'
+#' @return number
+#' @export
+#'
+#' @examples
+#' sum_squares(rnorm(50))
 sum_squares <- function(value) {
   sum((value - mean(value))^2)
 }
