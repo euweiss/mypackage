ee9d82657b1b29f713895264e04439e890cdaf3c
Eva Weiss <evaweiss88@gmail.com>
2020-01-07 13:55:16 +0000

    updates density plot

--- R/density_plot.R
+++ R/density_plot.R
@@ -1,9 +1,13 @@
+#' @importFrom ggplot2 ggplot aes geom_density
+#' @importFrom magrittr %>%
+#'
 density_plot <- function(v){
+  stopifnot(is.numeric(v))
   #this won't work without ggplot2
-  if(!requireNamespace("ggplot2", quietly = TRUE)) {
-    stop("Need ggplot2! Use install.packages(\"ggplot2\")")
-  }
-  ggplot2::ggplot(data.frame(values = v),
-                  ggplot2::aes(x = values)) +
-    ggplot2::geom_density()
+#  if(!requireNamespace("ggplot2", quietly = TRUE)) {
+#    stop("Need ggplot2! Use install.packages(\"ggplot2\")")
+#  }
+  data.frame(values = v) %>%
+    ggplot(aes(x = values)) +
+    geom_density()
 }
