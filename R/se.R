50ce6eae2e42525e237572516d43bb005221ab9f
Eva Weiss <evaweiss88@gmail.com>
2020-01-07 13:37:11 +0000

    fix se function

--- R/se.R
+++ R/se.R
@@ -3,14 +3,13 @@
 #' Returns the standard error
 #' from standard deviation and sample size
 #'
-#' @param s numeric value
-#' @param n numeric value (integer)
+#' @param value numeric vector
 #'
 #' @return number
 #' @export
 #'
 #' @examples
-#' se(5,200)
-se <- function(s,n){
-  s/(sqrt(n))
+#' se(rnorm(20))
+se <- function(value){
+  sd(value)/(sqrt(length(value)))
 }
